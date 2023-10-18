db30db4cf08512e4395a380ecba3b3a974943afa
RedShawn258 <maheshiiti2018@gmail.com>
2023-10-19 02:37:01 +0530

    final

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import tensorflow as tf
+from data_preprocess import preprocess_data
+
+# Load the saved model
+@st.cache(allow_output_mutation=True)
+def load_model(path):
+    model = tf.keras.models.load_model(path)
+    return model
+loaded_model = load_model('pos_char_token_model')
+
+def main():
+    html_temp = """<div style="background-color:tomato;padding:10px">
+                   <h2 style="color:white;text-align:center;">Split Up NLP</h2>
+                   </div>"""
+                   
+    st.markdown(html_temp,unsafe_allow_html=True)
+    input_text = st.text_input("Abstract text")
+    if st.button('Split Up'):
+        preprocessor = preprocess_data(input_text)
+        line_numbers, total_lines, sentences = preprocessor.prepare_dataset()
+        output_dict = preprocessor.get_predictions(line_numbers, total_lines, sentences, loaded_model)
+
+        for label in ['BACKGROUND','OBJECTIVE','METHODS','RESULTS','CONCLUSIONS']:
+            st.write('### {}\n'.format(label))
+            st.write('{} \n'.format(output_dict[label]))
+
+if __name__=='__main__':
+    main()
